b790c1a6a9d36bd30c7eb31104b09e07824b6333
murphy.huang <huang.chao@olaola.chat>
2022-08-02 16:13:29 +0800

    remove unecessary code

--- config_prod.py
+++ config_prod.py
@@ -3,6 +3,6 @@
 configs = {
     'oss': {},
     'wechat': {
-        'url': 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=77f444cc-534f-4522-908a-fd0ff34eab14'
+        'url': 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=<key>'
     },
 }
